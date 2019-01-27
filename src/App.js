0f07ffa325a322709f8a3e9d61480ab238ace8a0
Mika Saaranen <mika.saaranen@finn-id.fi>
2019-01-27 09:18:22 +0200

    made feature branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello there! we are <br/>asassaassassasasassasasaas
+            we made a new branch<br/>
             Edit <code>src/App.js</code> and save to reload. 
           </p>  
           <a
